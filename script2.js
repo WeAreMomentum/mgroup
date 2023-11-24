391409550331e5e9136c421400d3cfc7239fd49c
Jakubmomentum987 <148462704+Jakubmomentum987@users.noreply.github.com>
2023-11-24 11:52:24 +0100

    Update script2.js

--- script2.js
+++ script2.js
@@ -427,7 +427,7 @@ projectLinkBox.forEach((linkbox, index) => {
 
 new simpleParallax(paraImages, {
   overflow: true,
-  scale: 1.5
+  scale: 1
 });
 
 //$("[data-paroller-factor]").paroller(); 09.01. change
